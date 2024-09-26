2833f154dfb4db0f62a0836b514de5177b5ddad6
Raghad <ragadtalhi@gmail.com>
2024-09-26 11:45:22 +0300

    Initial Commit

---
+++ DEMO APP/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  DEMO APP
+//
+//  Created by Raghad Altalhi on 22/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            Image("95A05EC9-D2E1-47BD-9013-5AB0410AFA9E_4_5005_c")
+                .resizable()
+                .frame(width: 150, height: 150)
+                .cornerRadius(40)
+        
+        VStack(alignment: .leading) {
+            Text("Riyadh").font(.title)
+            Text("Capital of saudi Arabia").font(.callout)
+                .foregroundColor(.gray)
+            }
+            Spacer()
+            Text("🇸🇦")
+                .font(.largeTitle)
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DEMO APP/DEMO_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  DEMO_APPApp.swift
+//  DEMO APP
+//
+//  Created by Raghad Altalhi on 22/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct DEMO_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
